afd91406ece4596882c10c9b8af04eeb62d976db
Darkwa Bright <darkwabright90@gmail.com>
2025-06-12 16:04:06 +0000

    init

---
+++ primitiveTypes.ts
@@ -0,0 +1,25 @@
+//number type
+const num = 5;
+let y: number;
+y = 6
+console.log(y + num);
+
+//string type
+const person = "Bright";
+let newPerson: string;
+newPerson = "kobby"
+console.log(`first person is ${person} and second person is ${newPerson}`) 
+
+//boolean type 
+const isNew = true;
+let isOld: boolean;
+isOld = false;
+console.log(`${isNew} and ${isOld}`);
+
+//null type
+const user = null
+let newUser: string | null = null
+
+//undefined type
+const age = undefined;
+let newAge: number | undefined = undefined;
