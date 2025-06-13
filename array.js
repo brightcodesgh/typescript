c6deafc0a3bce55a23d94d3ddfce6e7ed02329af
Darkwa Bright <darkwabright90@gmail.com>
2025-06-13 15:25:35 +0000

    arrays and tuples

---
+++ array.js
@@ -0,0 +1,17 @@
+"use strict";
+let arr = [];
+arr = [1, 2, 3, 4, 5];
+const newArr = ["hello", "hi", "bye"];
+console.log(`Array Numbers: ${arr}\n Array Strings: ${newArr}`);
+//nested array
+let nesNumArr = [[1, 2], [3, 4], [5, 6]];
+const nesStringArr = [["i", "am"], ["Bright", "Darkwa"]];
+console.log(`Nested Array Numbers: ${nesNumArr}\n Nested String Array: ${nesStringArr}`);
+//Tuple
+const coord = [1, 2];
+console.log(coord[0]);
+let coords = [
+    [1, 2],
+    [-1, -2]
+];
+console.log(coords[0][1]);
---
+++ array.ts
@@ -0,0 +1,24 @@
+let arr: number[] = [];
+arr = [1,2,3,4,5];
+
+const newArr: string[] = ["hello", "hi", "bye"];
+
+console.log(`Array Numbers: ${arr}\n Array Strings: ${newArr}`);
+
+
+//nested array
+
+let nesNumArr: number[][] = [[1,2], [3,4], [5,6]];
+const nesStringArr: string[][] = [["i", "am"], ["Bright", "Darkwa"]];
+
+console.log(`Nested Array Numbers: ${nesNumArr}\n Nested String Array: ${nesStringArr}`);
+
+//Tuple
+const coord: [number,number] = [1,2];
+console.log(coord[0]);
+
+let coords: [number,number][] = [
+    [1,2],
+    [-1,-2]
+]
+console.log(coords[0][1]);
